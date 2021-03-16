43b8d72ff1b6863903b9aed107f0a026a7d196ee
Dumbleedore <lucas.pari@icloud.com>
2021-03-15 21:52:14 -0300

    Adicionado update e delete

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller,Get,Post } from '@nestjs/common';
+import { Body, Controller,Get,Post,Delete} from '@nestjs/common';
 
 import {CatsService} from './cats.service';
 
@@ -14,5 +14,12 @@ findCats(){
 insertCat(@Body('name')name : string,@Body('age')age : number ){
     return this.catsService.insertCat(name,age);
 }
-
+@Delete('removecat') // REMOVE CAT
+removeCat(@Body('id')id : string){
+    return this.catsService.deleteCat(id);
+}
+@Post('updateCat') // UPDATE CAT
+updateCat(@Body('id')id : string,@Body('name') name : string){
+    return this.catsService.updateCat(id,name);
+}
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -11,4 +11,18 @@ export class CatsService {
     findAllCats(){
         return this.cats;
     }
+    deleteCat(id : string){
+        this.cats.forEach((cat,index) =>{
+            if(cat.id == id){
+                this.cats.splice(index,1);
+            }
+        })
+    }
+    updateCat(id : string,name : string){
+        this.cats.forEach((cat) =>{
+            if(cat.id == id){
+                cat.name = name;
+            }
+        })
+    }
 }
